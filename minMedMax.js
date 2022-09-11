01b0e8b61bc08513ae7c4baf5fc95512656aa6ff
FRIXERIA <pattharaphon.saef@kmutt.ac.th>
2022-09-11 10:27:34 +0700

    Update#1

--- minMedMax.js
+++ minMedMax.js
@@ -1,6 +1,20 @@
 const { template } = require('@babel/core')
-
+let min
+let med 
+let max
 function minMedMax(n1, n2, n3) {
-//test git
+    if ((n1>=n2)&&(n2>=n3)) {
+        return {min:n3, med:n2, max:n1}
+    } else if((n1>=n3)&&(n3>=2)){
+        return {min:n2, med:n3, max:n1}
+    }else if ((n2>=n1)&&(n1>=n3)) {
+        return {min:n3, med:n1, max:n2}
+    }else if ((n2>=n3)&&(n3>=n1)) {
+        return {min:n1, med:n3,max:n2}
+    }else if ((n3>=n2)&&(n2>=1)) {
+        return {min:n1,med:n2, max:n3}
+    }else if ((n3>=n1)&&(n1>=2)) {
+       return {min:n2,med:n1, max:n3}
+    }
 }
 module.exports = minMedMax
